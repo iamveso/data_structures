da6ae7d235a0a261ea539df276b0a9b90d2d6966
Simon Akpoveso <simon.akpoveso@globalaccelerex.com>
2022-04-27 13:20:14 +0100

    Added new prepend test numbers

--- c_lang/LInkedLists/src/main.c
+++ c_lang/LInkedLists/src/main.c
@@ -7,15 +7,23 @@ int main() {
     {
         printf("%p\n", list);
     }
-    int val[4] = {1,2,3,4};
+    int val[] = {1,2,3,4,5,6,7,8};
     int* a = &val[0];
     int* b = &val[1];
     int* c = &val[2];
     int* d = &val[3];
+    int* e = &val[4];
+    int *f = &val[5];
+    int* g = &val[6];
+    int* h = &val[7];
     prepend(list, a);
     prepend(list, b);
     prepend(list, c);
     prepend(list, d);
+    prepend(list, e);
+    prepend(list, f);
+    prepend(list, g);
+    prepend(list, h);
     print_int_list(list);
     return 0;
 }
